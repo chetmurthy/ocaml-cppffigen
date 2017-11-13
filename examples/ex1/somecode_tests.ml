71c751ea7030af278d4a672e42ccdec8cc43c910
Chet Murthy <chetsky@gmail.com>
2017-11-12 22:55:18 -0800

    more cleanup (rocksdb fully removed)

--- examples/ex1/somecode_tests.ml
+++ examples/ex1/somecode_tests.ml
@@ -6,7 +6,7 @@ let all = "all_tests" >:::
   [
     "somecode_foo" >::
       (fun ctxt ->
-	assert_equal "45" (Ocaml_rocksdb.somecode_foo 45)) ;
+	assert_equal "45" (Ocaml_somecode.somecode_foo 45)) ;
   ]
   
 (* Run the tests in test suite *)
