40d50fc89490bf90174c9bd985cda14af22e1879
Chet Murthy <chetsky@gmail.com>
2025-02-09 20:23:02 -0800

    meh

--- cppffi.h
+++ cppffi.h
@@ -67,33 +67,41 @@ value c2ml(sentinel_GENERIC& unused, const std::string& v) ;
 
 void ml2c(const value v, std::string *cv) ;
 
-template<class T, class U>
-  value c2ml(sentinel_GENERIC& unused, const T& t, const U& u) ;
-template<class T, class U>
-  value c2ml(sentinel_GENERIC& unused, const std::tuple<T, U>& v) ;
+template<class T, class U, typename ML_T, typename ML_U>
+value c2ml(ML_T& unused1, ML_U& unused2, const T& t, const U& u) ;
+template<class T, class U, typename ML_T, typename ML_U>
+value c2ml(sentinel_TUPLE2<ML_T, ML_U>& unused, const std::tuple<T, U>& v) ;
 
 template<class T, class U>
   void ml2c(const value v, std::tuple<T, U>* cv) ;
 
-template<class T, class U, class V>
-  value c2ml(sentinel_GENERIC& unused, const T& t, const U& u, const V& v) ;
+template<class T, class U, class V,
+	 typename ML_T,
+	 typename ML_U,
+	 typename ML_V
+        >
+value c2ml(ML_T& unusedt, ML_U& unusedu, ML_V& unusedv, const T& t, const U& u, const V& v);
 
-template<class T, class U, class V>
-  value c2ml(sentinel_GENERIC& unused, const std::tuple<T, U, V>& v) ;
+template<class T, class U, class V,
+	 typename ML_T,
+	 typename ML_U,
+	 typename ML_V
+        >
+value c2ml(sentinel_TUPLE3<ML_T, ML_U, ML_V>& unused, const std::tuple<T, U, V>& v) ;
 
 template<class T, class U, class V>
   void ml2c(const value v, std::tuple<T, U, V>* cv) ;
 
-template<class T>
-value c2ml(sentinel_GENERIC& unused, const std::vector<T>& v) ;
+template<class T, typename ML_T>
+value c2ml(sentinel_ARRAY<ML_T> unused, const std::vector<T>& v) ;
 
 template<typename T>
 void ml2c(const value v, std::vector<T>* cv) ;
 
-template<class T>
-value c2ml_opt(sentinel_GENERIC& unused, const T *&p) ;
+template<class T, typename ML_T>
+value c2ml_opt(ML_T& unused, const T *&p) ;
 
-template <class T>
-value c2ml(sentinel_GENERIC& unused, const Opt<T> *ssp) ;
+template <class T, typename ML_T>
+value c2ml(sentinel_OPTION<ML_T> unused, const Opt<T> *ssp) ;
 
 #endif // cppffigen_h_included
