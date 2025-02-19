f394272703b131c1e38c6b71144e3c433b0baca5
Chet Murthy <chetsky@gmail.com>
2025-02-19 13:37:29 -0800

    conversion of different-sized C and ML types

--- examples/ex1/somecode_tests.ml
+++ examples/ex1/somecode_tests.ml
@@ -39,8 +39,10 @@ let all = "all_tests" >:::
       ; assert_equal None (somecode_roundtrip_string_option None)
       ; assert_equal (Some "foo") (somecode_roundtrip_string_option (Some "foo"))
       ; assert_equal ST0.{b=true;uc='a'} (somecode_roundtrip_ST0_t ST0.{b=true;uc='a'})
-      ; let st = ST.{ b = true ;	uc = 'a' ; n = 1 ; nL = 1L ; s = "foo" ; sz = 0xdeadbeefdeadbeefL } in
-        assert_equal st (somecode_roundtrip_ST_t st)
+      ; (let st = ST.{ b = true ;	uc = 'a' ; n = 1 ; nL = 1L ; s = "foo" ; sz = 0xdeadbeefdeadbeefL } in
+         assert_equal st (somecode_roundtrip_ST_t st))
+      ; assert_equal 1L (somecode_roundtrip_short_t 1L)
+      ; assert_equal 0L (somecode_roundtrip_short_t 65536L)
       )
   ]
   
